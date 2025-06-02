f800a32c3cc0db0781ace850c76d547725bc6098
Максим <150716393+MaxTop60@users.noreply.github.com>
2025-06-02 23:25:28 +0300

    Delete frontend directory

--- frontend/src/helpers/ScrollToHeader.js
+++
@@ -1,7 +0,0 @@
-const sqrollToHeader = () => {
-    const header = document.querySelector('.header');
-    header.scrollIntoView({behavior: 'smooth'});
-    return false;
-}
-
-export default sqrollToHeader;
