9fcd9b430e182973af9c5ef41f9f599c84d83468
Максим <150716393+MaxTop60@users.noreply.github.com>
2025-06-03 03:06:21 +0300

    Update urls.py

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -39,8 +39,4 @@ urlpatterns = [
     path("already_view/", AlreadyViewView.as_view())
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-else:
-    # Для production на Render
-    urlpatterns += [re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT})]
+urlpatterns += [re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT})]
